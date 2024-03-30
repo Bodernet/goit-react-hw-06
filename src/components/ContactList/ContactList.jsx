38cddb47164e664345253f265be63b98bd7d40a4
Bodernet <142262665+Bodernet@users.noreply.github.com>
2024-03-30 14:56:45 +0200

    продовження

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -8,6 +8,10 @@ const ContactList = () => {
   const contacts = useSelector(selectContacts);
   const nameFilter = useSelector(selectNameFilter);
 
+  //   const filteredContacts = contacts.filter((contact) =>
+  //   contact.name.toLowerCase().includes(nameFilter.toLowerCase())
+  // ); ТУТ ПРОБЛЕМКА
+
   const filteredContacts = contacts
     ? contacts.filter((contact) =>
         contact.name.toLowerCase().includes(nameFilter.toLowerCase())
